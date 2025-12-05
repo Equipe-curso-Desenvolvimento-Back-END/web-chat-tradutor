3a1b802eea87762eed26b92ab6d05fde13615e81
DevPapito <pietroeziosuzano@gmail.com>
2025-12-05 09:50:29 -0300

    Organização de codigo, criação do arquivo IndexController, alteração de formato subdiretorio /home para convenção index

--- src/main/java/com/chat/chat_tradutor/controller/HomeController.java
+++ src/main/java/com/chat/chat_tradutor/controller/HomeController.java
@@ -6,23 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    // Redirecionando por natureza ao /home
-
-    @GetMapping("/")
-    public String root() {
-
-        return "redirect:/home"; // home.html
-
-    }
-
     // Direcionando metodos para desenho dos arquivos .html
-
-    @GetMapping("/home")
-    public String home() {
-
-        return "home";
-
-    }
 
     @GetMapping("/login")
     public String login() {
